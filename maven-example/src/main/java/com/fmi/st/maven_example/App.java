f9ec089cb05fd5d4d0e0ec2f9032e066e2a41cbd
fmi <fmi@fmi-346-12.fmi.lcl>
2018-04-28 11:35:50 +0300

    goodbye from GIT

--- maven-example/src/main/java/com/fmi/st/maven_example/App.java
+++ maven-example/src/main/java/com/fmi/st/maven_example/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! - from GIT" );
+
+        System.out.println( "Hello World! - Goodbye from GIT" );
     }
 }
