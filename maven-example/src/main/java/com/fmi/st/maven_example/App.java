6fde7c7b18c0f940ae750f4566f3c874dc44194c
fmi <fmi@fmi-346-12.fmi.lcl>
2018-04-28 11:17:42 +0300

    Create new project

---
+++ maven-example/src/main/java/com/fmi/st/maven_example/App.java
@@ -0,0 +1,13 @@
+package com.fmi.st.maven_example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ maven-example/src/test/java/com/fmi/st/maven_example/AppTest.java
@@ -0,0 +1,21 @@
+package com.fmi.st.maven_example;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+	@Test
+    public void testAppTest()
+    {
+        
+    }
+
+}
